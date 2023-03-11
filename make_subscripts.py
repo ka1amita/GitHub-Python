e42fe663a393f3a82a517dfd54432e021797ea00
ka1amita <kalamatej@gmail.com>
2023-03-11 10:58:36 +0100

    renamed files
    added "_" and "s"

--- make_subscripts.py
+++ make_subscripts.py
@@ -7,7 +7,7 @@
 # logging set-up
 
 import logging
-logging.basicConfig(filename='makeSubscripts.log', filemode='w', encoding='utf-8', format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+logging.basicConfig(filename='make_subscripts.log', filemode='w', encoding='utf-8', format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 logging.info("start of program")
 logging.info("")
 
